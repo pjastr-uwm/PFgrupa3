05aaf8e7dbfdd1ad7ff111867968bd3e0a28c70a
pjastr-uwm <114875756+pjastr-uwm@users.noreply.github.com>
2025-02-28 12:44:31 +0100

    lab1 zad1

---
+++ lab01/tests/test_calculator.py
@@ -0,0 +1,21 @@
+import unittest
+from src.calculator import Calculator
+
+
+class TestCalculator(unittest.TestCase):
+
+    def setUp(self):
+        self.calculator = Calculator()
+
+    def test_sum_two_ints(self):
+        self.assertEqual(self.calculator.add(5, -9), -4)
+
+    def test_sum_two_ints_negative(self):
+        self.assertNotEqual(self.calculator.add(4, 5), -4)
+
+    def tearDown(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
