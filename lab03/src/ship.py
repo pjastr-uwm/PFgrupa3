a48f2ed433c93d229c1ca4e961fbb7ee0818f30c
pjastr <114875756+pjastr-uwm@users.noreply.github.com>
2025-03-14 12:38:33 +0100

    etap6

--- lab03/src/ship.py
+++ lab03/src/ship.py
@@ -3,6 +3,13 @@ class Ship:
     def __init__(self, destination, voyage_duration):
         self.destination = destination
         self.voyage_duration = voyage_duration
+        self.crew = []
 
     def calculate_fuel(self):
         return 100 * self.voyage_duration
+
+    def add_crew_member(self, param):
+        if param == "":
+            raise ValueError("Crew member name cannot be empty")
+
+        self.crew.append(param)
--- lab03/tests/test_ship.py
+++ lab03/tests/test_ship.py
@@ -22,6 +22,22 @@ class TestShipInitialization(unittest.TestCase):
         ship2 = Ship("Singapore", 5)
         self.assertEqual(ship2.calculate_fuel(), 500)
 
+    def test_add_crew_member(self):
+        ship1 = Ship("Rotterdam", 7)
+        ship1.add_crew_member("Captain Smith")
+        self.assertIn("Captain Smith", ship1.crew)
+        ship1.add_crew_member("First Mate Jones")
+        self.assertIn("First Mate Jones", ship1.crew)
+        ship1.add_crew_member("Engineer Roberts")
+        self.assertIn("Engineer Roberts", ship1.crew)
+
+    def test_add_empty_crew_member(self):
+        ship1 = Ship("Rotterdam", 7)
+        with self.assertRaises(ValueError) as context:
+            ship1.add_crew_member("")
+
+        self.assertEqual(str(context.exception), "Crew member name cannot be empty")
+
     def tearDown(self):
         pass
 
