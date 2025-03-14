6b4013c985c1748c17ccb5ed12a6d59747381084
pjastr <114875756+pjastr-uwm@users.noreply.github.com>
2025-03-14 11:47:04 +0100

    etap1

---
+++ lab03/tests/test_ship.py
@@ -0,0 +1,23 @@
+import unittest
+from src.ship import Ship
+
+class TestShipInitialization(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_creation(self):
+        ship1 = Ship("Rotterdam", 7)
+        self.assertIsInstance(ship1, Ship)
+
+    def test_initialization(self):
+        ship1 = Ship("Rotterdam", 7)
+        self.assertEqual(ship1.destination, "Rotterdam")
+        self.assertEqual(ship1.voyage_duration, 7)
+
+    def tearDown(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
