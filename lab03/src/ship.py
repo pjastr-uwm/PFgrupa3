af4430ccbabff616d62b0df98288eb86cff6147d
pjastr <114875756+pjastr-uwm@users.noreply.github.com>
2025-03-14 12:11:28 +0100

    etap 4

--- lab03/src/ship.py
+++ lab03/src/ship.py
@@ -3,3 +3,6 @@ class Ship:
     def __init__(self, destination, voyage_duration):
         self.destination = destination
         self.voyage_duration = voyage_duration
+
+    def calculate_fuel(self):
+        return 100 * self.voyage_duration
--- lab03/tests/test_ship.py
+++ lab03/tests/test_ship.py
@@ -16,6 +16,12 @@ class TestShipInitialization(unittest.TestCase):
         self.assertEqual(ship1.destination, "Rotterdam")
         self.assertEqual(ship1.voyage_duration, 7)
 
+    def test_calculate_fuel(self):
+        ship1 = Ship("Rotterdam", 7)
+        self.assertEqual(ship1.calculate_fuel(), 700)
+        ship2 = Ship("Singapore", 5)
+        self.assertEqual(ship2.calculate_fuel(), 500)
+
     def tearDown(self):
         pass
 
